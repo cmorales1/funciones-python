186edc6ec0a0a1b0fe3ebf553cca08397879f710
cmorales1 <32424023+cmorales1@users.noreply.github.com>
2018-01-27 14:10:51 -0600

    funciones

---
+++ prueba2.py
@@ -0,0 +1,22 @@
+#funciones#----------------------
+def Nombre():
+	print "Cesar"
+	Apellido()
+	
+
+def Apellido():
+	Apellido="Morales"
+	print Apellido
+	Edad()
+		
+
+def Edad():
+	Edad=19
+	print Edad
+
+Nombre()
+
+
+
+
+
